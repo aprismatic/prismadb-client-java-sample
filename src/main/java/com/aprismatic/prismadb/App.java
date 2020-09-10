4742bafd7a175dbc2cf96dc26c4047e90ab705a1
Calvin Che <cczy@aprismatic.com>
2020-09-11 01:36:07 +0800

    Sample for encrypting a column

--- src/main/java/com/aprismatic/prismadb/App.java
+++ src/main/java/com/aprismatic/prismadb/App.java
@@ -22,23 +22,35 @@ public final class App {
                     + "e VARCHAR(30));");
 
             // Insert data
-            conn.executeQuery("INSERT INTO test_table (a, b, c, d, e) VALUES "
-                    + "(1, 2, 3, 'Hello', 'Prisma/DB'), "
-                    + "(12, 0, 7, 'Test', 'data'), "
-                    + "( 0, 2, 123, 'This is encrypted', 'And this is not'), "
-                    + "(71, 67, 13, 'Last', 'row');");
+            for (int i=0; i<200; i++) {
+                conn.executeQuery("INSERT INTO test_table (a, b, c, d, e) VALUES "
+                        + "(1, 2, 3, 'Hello', 'Prisma/DB'), "
+                        + "(12, 0, 7, 'Test', 'data'), "
+                        + "( 0, 2, 123, 'This is encrypted', 'And this is not'), "
+                        + "(71, 67, 13, 'Last', 'row');");
+            }
 
-            // Select data
-            ResultSet columnRes = conn
-                    .executeQuery("SELECT a, b, a + b, b * a, c, d, e FROM test_table WHERE b = 2 OR d LIKE 'Te%';");
-            while (columnRes.next()) {
-                System.out.println(columnRes.getString(1) + ", " + columnRes.getString(2) + ", "
-                        + columnRes.getString(3) + ", " + columnRes.getString(4) + ", " + columnRes.getString(5) + ", "
-                        + columnRes.getString(6) + ", " + columnRes.getString(7));
+            // Encrypt column
+            conn.executeQuery("PRISMADB ENCRYPT test_table.c FOR (MULTIPLICATION, ADDITION, SEARCH);");
+
+            // Check for encrypt status
+            while (true) {
+                // Prisma/DB requires a very small amount of time to start the encryption,
+                // therefore there is a short sleep before checking the status.
+                // This sleep is in the loop to not flood the console with status prints.
+                Thread.sleep(100);
+                ResultSet statusRes = conn
+                        .executeQuery("PRISMADB ENCRYPT test_table.c FOR (MULTIPLICATION, ADDITION, SEARCH) STATUS;");
+                statusRes.next();
+                String status = statusRes.getString(2);
+                System.out.println(status);
+                if (!status.equals("In Progress")) {
+                    break;
+                }
             }
 
             conn.close();
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
